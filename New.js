eb9549feb44a130aa46987167b955e607fb62480
Aashish Taili <140081359+Aashish-Taili@users.noreply.github.com>
2024-06-03 15:26:00 +0000

    Javascript practice

---
+++ New.js
@@ -0,0 +1,40 @@
+// function userlogin(username= "You"){
+//     if(!username){
+//         console.log("Please enter your Username")
+//         return
+
+//     }
+//     return `${username} just logged in to website`
+// }
+// console.log(userlogin("Aashish"))
+
+// function Cart(...num){
+//     return(num)
+
+// }
+// console.log(Cart(45896,45746,48756, 11,2,34,5478,6945,1489,3594,6485));
+
+// const User={
+//     username: "Aashish-Taili",
+//     price:399
+// }
+
+// function shoes(user){
+//     console.log(`${user.username} bought  shoes of ₹${user.price}`);
+// }
+
+// shoes({
+//     username:"Rajmal Taili",
+//     price: 499
+// })
+
+
+// // const array=['A','B','C','D','E','F','G']
+// function arr(newarr){
+//     return newarr[5]
+  
+// }
+// // console.log(arr(array));
+// console.log(arr(['A','B','C','D','E','F','G']));
+
+
